10b6253741a68729c3369dfcd44cd32a7a5c1ac4
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-12-01 13:19:24 +0100

    Complete first function

--- exJs/05-tabella-dinamica/contatti.js
+++ exJs/05-tabella-dinamica/contatti.js
@@ -9,6 +9,78 @@ const datiTabella = [{
     "Argomenti": "PHP"
 }]
 
-function stringaToID(stringa){
+function stringaToID(stringa) {
     return stringa.toLowerCase().replace(/[^a-zA-Z]/g, "");
 }
+
+const section = document.querySelector("section")
+
+function displayTable() {
+    let table =
+        `<table>
+            <tr>
+                <th id="autore">Autore</th><th id="email">Email</th><th id="argomenti">Argomenti</th>
+            </tr>`;
+    for (let i = 0; i < datiTabella.length; i++) {
+        let autore = datiTabella[i]["Autore"];
+        let email = datiTabella[i]["Email"];
+        let argomenti = datiTabella[i]["Argomenti"];
+        let id = email.split("@")[0];
+
+        table += `
+                <tr>
+                    <th id="${id}">${autore}</th><td headers="email ${id}">${email}</td><td headers="argomenti ${id}">${argomenti}</td>
+                </tr>
+                `;
+    }
+    table += `</table>`;
+    section.innerHTML += table;
+}
+
+function displayAccessibleTable() {
+    let table =
+        `<table>
+            <tr>
+                <th id="autore">Autore</th><th id="email">Email</th><th id="argomenti">Argomenti</th>
+            </tr>`;
+    for (let i = 0; i < datiTabella.length; i++) {
+        let autore = datiTabella[i]["Autore"];
+        let email = datiTabella[i]["Email"];
+        let argomenti = datiTabella[i]["Argomenti"];
+        let id = email.split("@")[0];
+
+        table += `
+                <tr>
+                    <th id="${id}">${autore}</th><td headers="email ${id}">${email}</td><td headers="argomenti ${id}">${argomenti}</td>
+                </tr>
+                `;
+    }
+    table += `</table>`;
+    section.innerHTML += table;
+}
+
+function displayFullFledgedTable() {
+    let table =
+        `<table>
+            <tr>
+                <th id="autore">Autore</th><th id="email">Email</th><th id="argomenti">Argomenti</th>
+            </tr>`;
+    for (let i = 0; i < datiTabella.length; i++) {
+        let autore = datiTabella[i]["Autore"];
+        let email = datiTabella[i]["Email"];
+        let argomenti = datiTabella[i]["Argomenti"];
+        let id = email.split("@")[0];
+
+        table += `
+                <tr>
+                    <th id="${id}">${autore}</th><td headers="email ${id}">${email}</td><td headers="argomenti ${id}">${argomenti}</td>
+                </tr>
+                `;
+    }
+    table += `</table>`;
+    section.innerHTML += table;
+}
+
+displayTable();
+displayAccessibleTable();
+displayFullFledgedTable();
