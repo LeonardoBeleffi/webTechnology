adf18ad6ab66f1105a1f06f71fe7aaa30744f22c
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-12-02 01:02:59 +0100

    Add input check

--- preparazioneCompito/preparazionePhp/leonardo.beleffi@studio.unibo.it.php
+++ preparazioneCompito/preparazionePhp/leonardo.beleffi@studio.unibo.it.php
@@ -1,3 +1,12 @@
 <?php
 
+$a = $_GET["A"];
+$b = $_GET["B"];
+$o = $_GET["O"]; 
+
+if (!isset($a) || !isset($b) || !isset($o)) {
+    return;
+}
+echo "A: " . $a . ", B: " . $b . ", O: " . $o .".";
+
 ?>
