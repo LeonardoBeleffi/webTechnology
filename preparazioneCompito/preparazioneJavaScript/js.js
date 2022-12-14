b60c025b63a988e096e0c762b171ef1dee3a855e
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-12-14 22:50:38 +0100

    Decorative fix

--- preparazioneCompito/preparazioneJavaScript/js.js
+++ preparazioneCompito/preparazioneJavaScript/js.js
@@ -1,9 +1,6 @@
 const images = Array.from(document.querySelectorAll("img"));
 
 window.onload = function() {
-    // Vengano nascoste tutte le immagini, eccetto le prime due.
-    // Alla prima immagine deve essere inoltre aggiunta la classe "current".
-
     let len = images.length;
     if (len < 1)
         return;
@@ -18,11 +15,6 @@ window.onload = function() {
 }
 
 function click(e) {
-    // Al click su un'immagine, si dovrà controllare se l’immagine ha la classe current e nel caso non fare nulla.
-    // In caso contrario, invece, bisognerà aggiungere la classe current, rimuovendola da altre immagini.
-    // Successivamente, andranno opportunamente nascoste e visualizzate le immagini in modo che siano visibili:
-    // l’immagine con classe current, l’eventuale immagine prima e l’eventuale immagine dopo.
-
     let img = e.target;
     let indexOfCurrentImage = images.indexOf(img);
 
